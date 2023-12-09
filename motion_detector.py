ff079a8c2452ef6369ad3bb2057608707328d274
Adam Tovatt <sporedenstore@hotmail.com>
2023-12-09 13:35:17 +0100

    use led to indicate motion

--- motion_detector.py
+++ motion_detector.py
@@ -1,4 +1,5 @@
 from machine import Pin
+from on_board_led import OnBoardLed
 import time
 
 class MotionDetector:
@@ -6,11 +7,13 @@ class MotionDetector:
         # Store the pin number as a field
         self.pin = Pin(pinNumber, Pin.IN, Pin.PULL_DOWN)
         self.last_motion_time = 0  # Variable to store the time of the last motion
+        self.led = OnBoardLed()
 
     def waitForMotion(self, motion_interval=None):
         while True:
             time.sleep(1)
             if self.pin.value() == 1:
+                self.led.on()
                 current_time = time.time()
                 # Check if enough time has passed since the last motion
                 if motion_interval is None or (current_time - self.last_motion_time) >= motion_interval:
@@ -20,3 +23,5 @@ class MotionDetector:
                 else:
                     # If not enough time has passed, continue waiting
                     time.sleep(1)
+            else:
+                self.led.off()
