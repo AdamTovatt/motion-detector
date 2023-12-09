c752878ee7f176ad12913faf9aba252c26598e2c
Adam Tovatt <sporedenstore@hotmail.com>
2023-12-09 15:01:59 +0100

    use analog pin input

--- motion_detector.py
+++ motion_detector.py
@@ -1,18 +1,19 @@
-from machine import Pin
+from machine import Pin, ADC
 from on_board_led import OnBoardLed
 import time
 
 class MotionDetector:
     def __init__(self, pinNumber):
         # Store the pin number as a field
-        self.pin = Pin(pinNumber, Pin.IN, Pin.PULL_DOWN)
+        self.pin = ADC(Pin(pinNumber))
         self.last_motion_time = 0  # Variable to store the time of the last motion
         self.led = OnBoardLed()
 
     def waitForMotion(self, motion_interval=None):
         while True:
-            time.sleep(1)
-            if self.pin.value() == 1:
+            time.sleep(0.2)
+            motion = (self.pin.read_u16() > 1000)
+            if motion:
                 self.led.on()
                 current_time = time.time()
                 # Check if enough time has passed since the last motion
@@ -20,8 +21,5 @@ class MotionDetector:
                     # Update the last motion time
                     self.last_motion_time = current_time
                     return
-                else:
-                    # If not enough time has passed, continue waiting
-                    time.sleep(1)
             else:
                 self.led.off()
